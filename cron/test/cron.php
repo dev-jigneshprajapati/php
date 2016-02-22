d4fcee7396282f67e3fcf91b6e1390b1c58c39df
Jignesh Prajapati <jprajapati@happydesk.com>
2016-02-22 12:50:19 +0530

    solve error

--- cron/test/cron.php
+++ cron/test/cron.php
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-<?php
-echo "this is my first time create a git repository";
-echo "test";
-
-if($_GET)
-{
-	$argument1 = $_GET['arg1'];
-	$argument2 = $_GET['arg2'];
-}else{
-	$argument1 = 10;
-	$argument2 = 20;
-}
-
-$result = $argument1 + $argument2;
-
-echo  "Total of $argument1 and $argument2 is $result";
-
-?>
-=======
 <?php
 echo "this is my first time create a git repository";
 echo "test2";
@@ -36,4 +16,3 @@ $result = $argument1 + $argument2;
 echo  "Total of $argument1 and $argument2 is $result";
 
 ?>
->>>>>>> a81a081fc26f7efc0da44b68afbb56c7a9a8cc9c
